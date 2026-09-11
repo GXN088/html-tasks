7d06dc9e43bb7d6be733f41d52aeb370fd7bcd64
GXN088 <124631889+GXN088@users.noreply.github.com>
2026-09-11 08:04:41 +0300

    Create script.js

---
+++ open-modal/script.js
@@ -0,0 +1,7 @@
+function openModal(modalId) {
+  // Show the overlay
+  document.getElementById("overlay").style.display = "block";
+  
+  // Show the modal
+  document.getElementById(modalId).style.display = "block";
+}
