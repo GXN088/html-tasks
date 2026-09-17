7198357b11a558e5e588412675952b858c60085d
GXN088 <124631889+GXN088@users.noreply.github.com>
2026-09-17 20:13:07 +0300

    Create script.js

---
+++ onclick/script.js
@@ -0,0 +1,9 @@
+// Функция для открытия бокового меню
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+// Функция для закрытия бокового меню
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
